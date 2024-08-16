6125d1c4ad22c34bdc697125a79ffdd2ec8acfd9
Ishant Bhatt <ishantbhatt1654@gmail.com>
2024-08-16 15:34:59 +0530

    changes:sports section optimization

--- src/app/Components/SportsCard.jsx
+++ src/app/Components/SportsCard.jsx
@@ -7,16 +7,23 @@ function SportsCard({ key, name, img, onHover }) {
     <div className="w-full flex justify-center md:justify-end ">
       <div
         className="w-[80%]  rounded-s-2xl rounded-e-2xl md:rounded-e-none  py-12 pl-10 hover:w-[85%] transition-all duration-500 ease-in-out bg-cover bg-center bg-no-repeat grayscale hover:grayscale-0 bg-black"
-        style={{
-          backgroundImage: `url(${img})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-        }}
+        // style={{
+        //   backgroundImage: `url(${img})`,
+        //   backgroundSize: "cover",
+        //   backgroundPosition: "center",
+        //   backgroundRepeat: "no-repeat",
+        // }}
         onMouseEnter={() => onHover(name)}
         onMouseLeave={() => onHover(null)}
       >
-        <p1 className="text-white font-bold text-2xl">{name}</p1>
+        <Image
+          src={img}
+          alt={name}
+          layout="fill"
+          objectFit="cover"
+          className="absolute inset-0 rounded-s-2xl"
+        />
+        <p1 className="text-white font-bold text-2xl relative z-10">{name}</p1>
       </div>
     </div>
   );
